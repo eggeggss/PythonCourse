8e28dbcb39b8124a4fb5e900d27734355ac5a2eb
eggeggss <eggeggss@gmail.com>
2017-11-12 23:55:58 +0800

    1112v2

--- Ch04/main.py
+++ Ch04/main.py
@@ -24,9 +24,11 @@ print ("*******")
 print (os.path.abspath(__file__))
 #取得目前目錄路徑
 print (os.path.dirname(os.path.abspath(__file__)))
+#取得檔名 or 路徑
+print (os.path.basename(os.path.abspath(__file__)))
+print (os.path.basename(os.path.dirname(os.path.abspath(__file__))))
+
 
 #執行dos指令
-os.system("ifconfig")
-
-os.system("mkdir origin")
-os.system("cp main.py myDir")
+#os.system("ifconfig")
+#os.system("cp main.py myDir")
--- Ch04/myDir/main.py
+++ Ch04/myDir/main.py
@@ -24,9 +24,8 @@ print ("*******")
 print (os.path.abspath(__file__))
 #取得目前目錄路徑
 print (os.path.dirname(os.path.abspath(__file__)))
+print (os.path.abspath(__file__))
 
 #執行dos指令
-os.system("ifconfig")
-
-os.system("mkdir origin")
+#os.system("ifconfig")
 os.system("cp main.py myDir")
