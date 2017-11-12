4f79e8ffadc98127ef9058df2c333e3a077f7326
eggeggss <eggeggss@gmail.com>
2017-11-12 23:35:20 +0800

    1112

---
+++ Ch04/main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+file="myFile.txt"
+
+if os.path.exists(file):
+    os.remove(file)
+    
+    
+else:
+    print("檔案未建立")
+    #建立目錄
+    if not (os.path.exists("myDir")):
+       os.mkdir("myDir")
+
+#os.mkdir() 建立目錄
+#os.rmdir() 刪除目錄
+#os.remove() 刪除檔案
+#os.path.exists() 判斷是否存在
+
+print ("*******")
+#取得目前檔案路徑
+print (os.path.abspath(__file__))
+#取得目前目錄路徑
+print (os.path.dirname(os.path.abspath(__file__)))
+
+#執行dos指令
+os.system("ifconfig")
+
+os.system("mkdir origin")
+os.system("cp main.py myDir")
